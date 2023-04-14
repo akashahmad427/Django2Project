475d45769e3ce8075d91a07e87e6b2c1f3953eca
akash <ahmadakash427427@gmail.com>
2023-04-13 23:18:25 -0700

    first commit

---
+++ firstproject/enroll/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,HttpResponseRedirect
+from .forms import StudentRegistration
+from .models import User
+# Create your views here.
+def show_data(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            fm.save()
+            fm = StudentRegistration()
+    else:
+        fm = StudentRegistration()
+    stud = User.objects.all()
+    return render(request,'enroll/updatestudent.html',{'id':fm ,'data':stud})
+def delete(request,id):
+    if request.method == 'POST':
+        pi=User.objects.get(pk=id)
+        pi.delete()
+    return HttpResponseRedirect('/')
+
+def update(request,id):
+    if request.method == 'POST':
+        pi=User.objects.get(pk=id)
+        fm=StudentRegistration(request.POST,instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+         pi=User.objects.get(pk=id)
+         fm=StudentRegistration(instance=pi)   
+    return render(request,'enroll/addandshow.html',{'form' : fm})
+
